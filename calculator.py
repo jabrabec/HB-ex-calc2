a9a9cdbb71812a9d33b8b9f41019e82393da012b
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 17:26:15 -0700

    square & cube work; separated code by length of token list for applying int()

--- calculator.py
+++ calculator.py
@@ -13,16 +13,23 @@ while True:
     user_input = raw_input("> ")
     user_input = user_input.rstrip()
     tokens = user_input.split(" ")
-    tokens[1] = int(tokens[1])
-    tokens[2] = int(tokens[2])
-    if tokens[0] == "q":
-        break
-    elif tokens[0] == "+":
-        print add(tokens[1], tokens[2])
-    elif tokens[0] == "-":
-        print subtract(tokens[1], tokens[2])
-    elif tokens[0] == "*":
-        print multiply(tokens[1], tokens[2])
-    elif tokens[0] == "/":
-        print ("%.6f" % divide(tokens[1], tokens[2]))
-    # print tokens
+    if len(tokens) == 1:
+        if tokens[0] == "q":
+            break
+    elif len(tokens) == 2:
+        tokens[1] = int(tokens[1])
+        if tokens[0] == "square":
+            print square(tokens[1])
+        elif tokens[0] == "cube":
+            print cube(tokens[1])
+    elif len(tokens) == 3:
+        tokens[1] = int(tokens[1])
+        tokens[2] = int(tokens[2])
+        if tokens[0] == "+":
+            print add(tokens[1], tokens[2])
+        elif tokens[0] == "-":
+            print subtract(tokens[1], tokens[2])
+        elif tokens[0] == "*":
+            print multiply(tokens[1], tokens[2])
+        elif tokens[0] == "/":
+            print ("%.6f" % divide(tokens[1], tokens[2]))
