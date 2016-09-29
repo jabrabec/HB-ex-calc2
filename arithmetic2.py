ce351e3096be9812b9f6cec35bcd3d982e72c57c
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-29 12:45:28 -0700

    revised functions in new files to accept >2 numbers at user input, revised addition function

---
+++ arithmetic2.py
@@ -0,0 +1,37 @@
+def add(input_numbers):
+    sum_value = 0
+    for index in range(len(input_numbers)):
+        # print "index is:", index
+        sum_value += int(input_numbers[index])
+    return sum_value
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    # Need to turn at least argument to float for division to
+    # not be integer division
+    return float(num1) / float(num2) 
+
+
+def square(num1):
+    # Needs only one argument
+    return num1 * num1
+
+
+def cube(num1):
+    # Needs only one argument
+    return num1 * num1 * num1
+
+
+def power(num1, num2):
+    return num1 ** num2  # ** = exponent operator
+
+
+def mod(num1, num2):
+    return num1 % num2
