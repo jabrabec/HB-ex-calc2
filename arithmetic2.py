5e66a6ca23c47809574a4737b381fcc7f11af2cf
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-29 13:04:01 -0700

    commented out work-in-progress functions

--- arithmetic2.py
+++ arithmetic2.py
@@ -12,29 +12,38 @@ def subtract(input_numbers):
     return sub_value
 
 
-def multiply(num1, num2):
-    return num1 * num2
+# function does not yet accept multiple inputs
+# def multiply(num1, num2):
+#     sub_value = int(input_numbers[0])
+#     for index in range(1, len(input_numbers)):
+#         sub_value -= int(input_numbers[index])
+#     return sub_value
 
 
-def divide(num1, num2):
-    # Need to turn at least argument to float for division to
-    # not be integer division
-    return float(num1) / float(num2) 
+# function does not yet accept multiple inputs
+# def divide(num1, num2):
+#     # Need to turn at least argument to float for division to
+#     # not be integer division
+#     return float(num1) / float(num2) 
 
 
-def square(num1):
-    # Needs only one argument
-    return num1 * num1
+# function needs to integerize input
+# def square(num1):
+#     # Needs only one argument
+#     return num1 * num1
 
 
-def cube(num1):
-    # Needs only one argument
-    return num1 * num1 * num1
+# function needs to integerize input
+# def cube(num1):
+#     # Needs only one argument
+#     return num1 * num1 * num1
 
 
-def power(num1, num2):
-    return num1 ** num2  # ** = exponent operator
+# function does not yet accept multiple inputs
+# def power(num1, num2):
+#     return num1 ** num2  # ** = exponent operator
 
 
-def mod(num1, num2):
-    return num1 % num2
+# function does not yet accept multiple inputs
+# def mod(num1, num2):
+#     return num1 % num2
--- calculator2.py
+++ calculator2.py
@@ -16,21 +16,21 @@ def math_function():
         tokens = user_input.split()
         if tokens[0] == "q":
             return
-        elif tokens[0] == "square":
-            print square(tokens[1:])
-        elif tokens[0] == "cube":
-            print cube(tokens[1:])
+        # elif tokens[0] == "square":
+        #     print square(tokens[1:])
+        # elif tokens[0] == "cube":
+        #     print cube(tokens[1:])
         elif tokens[0] == "+":
             print add(tokens[1:])
         elif tokens[0] == "-":
             print subtract(tokens[1:])
-        elif tokens[0] == "*":
-            print multiply(tokens[1:])
-        elif tokens[0] == "/":
-            print ("%.6f" % divide(tokens[1:]))
-        elif tokens[0] == "pow":
-            print power(tokens[1:])
-        elif tokens[0] == "mod":
-            print mod(tokens[1:])
+        # elif tokens[0] == "*":
+        #     print multiply(tokens[1:])
+        # elif tokens[0] == "/":
+        #     print ("%.6f" % divide(tokens[1:]))
+        # elif tokens[0] == "pow":
+        #     print power(tokens[1:])
+        # elif tokens[0] == "mod":
+        #     print mod(tokens[1:])
 
 math_function()
