8aeed698b8316ea86e49496fc4fac5978fa68699
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 17:02:40 -0700

    addition function works; tokens are converted to integers

--- calculator.py
+++ calculator.py
@@ -13,5 +13,10 @@ while True:
     user_input = raw_input("> ")
     user_input = user_input.rstrip()
     tokens = user_input.split(" ")
-    print tokens
-    break
+    tokens[1] = int(tokens[1])
+    tokens[2] = int(tokens[2])
+    if tokens[0] == "q":
+        break
+    elif tokens[0] == "+":
+        print add(tokens[1], tokens[2])
+    # print tokens
